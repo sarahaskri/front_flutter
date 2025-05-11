54e54131167b72e15227b008315625572c2abd25
sarahaskri <sarahaskri00@gmail.com>
2025-05-12 00:03:47 +0100

    MODIF

--- fityes/lib/main.dart
+++ fityes/lib/main.dart
@@ -113,7 +113,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:AdminDashboardPage(),
+      home:ProgressTrackingPage(),
       navigatorKey: navigatorKey,
       routes: {
     
